ab999f3b7db57f07ad38c54b55099c5998a06441
Pamella Fernandes <88452990+pamellamoff@users.noreply.github.com>
2024-05-28 11:55:31 -0300

    Create esquema_apuracao.sql

---
+++ esquema_apuracao.sql
@@ -0,0 +1,33 @@
+--TRANSFERE TABELA DE METAS DE EMPRESTIMO
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_metas__individuais;
+
+INSERT INTO sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_metas__individuais
+  SELECT * FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_metas__individuais;
+
+--TRANSFERE TABELA DE PARTICIPANTES POR DIA UTIL        
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_apurac.int__participantes_dia_util;
+
+INSERT INTO sdx_excelencia_comercial.camp_incentivo__rede_apurac.int__participantes_dia_util   
+  SELECT * FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util;
+
+--TRANSFERE TABELA DE METAS DE DPZ
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_dpz__metas;
+
+INSERT INTO sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_dpz__metas
+  SELECT * FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_dpz__metas;
+
+--TRANSFERE TABELA DE METAS DE SERVIÇOS 
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_servicos_meta__geral;
+
+INSERT INTO sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_servicos_meta__geral   
+  SELECT * FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_servicos_meta__geral;
+
+--TRANSFERE TABELA DE PRODUÇÃO DE SERVIÇOS
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_servicos_prod__geral;
+
+INSERT INTO sdx_excelencia_comercial.camp_incentivo__rede_apurac.int_servicos_prod__geral   
+  SELECT * FROM sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_servicos_prod__geral;
+
+
+
+
