ac3274ab1f5c1d0ed6dffb8fa8bae3785814a5ff
Pamella Fernandes <88452990+pamellamoff@users.noreply.github.com>
2024-05-28 12:05:55 -0300

    Create esquema_vigente.sql

---
+++ esquema_vigente.sql
@@ -0,0 +1,15 @@
+
+--LIMPA TABELA DE PARTICIPANTES POR DIA UTIL   
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_vigente.int__participantes_dia_util;
+
+--LIMPA TABELA DE METAS DE EMPRESTIMO
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_metas__individuais;
+
+--LIMPA TABELA DE METAS DE DPZ
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_dpz__metas;
+
+--LIMPA TABELA DE METAS DE SERVIÇOS 
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_servicos_meta__geral;
+
+--LIMPA TABELA DE PRODUÇÃO DE SERVIÇOS
+TRUNCATE TABLE sdx_excelencia_comercial.camp_incentivo__rede_vigente.int_servicos_prod__geral;
