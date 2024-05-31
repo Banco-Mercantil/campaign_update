1d043654fada71699142bd2959e3219995b7d802
Pamella Fernandes <88452990+pamellamoff@users.noreply.github.com>
2024-05-31 14:10:21 -0300

    Update alter_table.sql

--- alter_table.sql
+++ alter_table.sql
@@ -105,7 +105,7 @@ SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__re
 
 
 
------------------------------------------------------CAMPANHA BEM AQUI----------------------------------------------
+----------------------------------------BEM AQUI---------------------------------------------
 
 --int_metas__individuais
     --camp_incentivo__bemaqui_vigente
@@ -116,13 +116,6 @@ SELECT meta_individ_crt_consig_dia FROM sdx_excelencia_comercial.camp_incentivo_
 ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.int_metas__individuais ADD COLUMN meta_cartao NUMBER(38,0);
 ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.int_metas__individuais ADD COLUMN meta_individ_crt_consig_dia NUMBER(38,0);
 SELECT meta_individ_crt_consig_dia FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.int_metas__individuais;
-    --camp_incentivo__bemaqui_historico
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.int_metas__individuais ADD COLUMN meta_cartao NUMBER(38,0);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.int_metas__individuais ADD COLUMN meta_individ_crt_consig_dia NUMBER(38,0);
-SELECT meta_individ_crt_consig_dia FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.int_metas__individuais; 
-
-
-
 
 
 --mrt_apuracao__individual
@@ -143,16 +136,13 @@ ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao
 ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao__individual ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
 SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao__individual;
     --camp_incentivo__bemaqui_historico
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN producao_efetivada_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN producao_desconsiderada_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN producao_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN meta_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN rlz_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
-SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__individual;
-
-
-
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN producao_efetivada_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN producao_desconsiderada_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN producao_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN meta_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN rlz_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
+SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__individual;
 
 
 --mrt_apuracao__agencia
@@ -173,13 +163,14 @@ ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao
 ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao__agencia ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
 SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_apurac.mrt_apuracao__agencia;
     --camp_incentivo__bemaqui_historico
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN producao_efetivada_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN producao_desconsiderada_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN producao_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN meta_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN rlz_crt_consignado NUMBER(10,2);
-ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
-SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.mrt_apuracao__agencia;
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN producao_efetivada_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN producao_desconsiderada_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN producao_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN meta_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN rlz_crt_consignado NUMBER(10,2);
+ALTER TABLE sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia ADD COLUMN rlz_elegibilidade_app_mb NUMBER(10,2);
+SELECT rlz_elegibilidade_app_mb FROM sdx_excelencia_comercial.camp_incentivo__bemaqui_historico.hst_apuracao__agencia;
+
 
 
 
